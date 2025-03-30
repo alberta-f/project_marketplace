ce8c13349244a31749390cf1a81eeb38c0cf420d
alberta-f <delyasalakhova@gmail.com>
2025-03-30 15:07:01 +0300

    delete mypy + bd refactoring

---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+from app.db.base import Base
+
+from .models import User
+
+__all__ = ["Base", "User"]
---
+++ app/models/models.py
@@ -0,0 +1,19 @@
+import datetime
+import uuid
+
+from sqlalchemy import Boolean, DateTime, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.db.base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
+    email: Mapped[str] = mapped_column(String(320), unique=True, nullable=False)
+    username: Mapped[str] = mapped_column(String(100), nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(128), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    created_at: Mapped[datetime.datetime] = mapped_column(DateTime,
+                                                          default=datetime.datetime.utcnow)
