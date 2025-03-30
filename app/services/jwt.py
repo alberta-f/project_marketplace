085eb65127fc4707c3e4c9db52699192cabc8e49
alberta-f <delyasalakhova@gmail.com>
2025-03-30 20:02:50 +0300

    store-token

--- app/services/jwt.py
+++ app/services/jwt.py
@@ -5,9 +5,10 @@ from uuid import UUID
 from jose import JWTError, jwt
 
 from app.config import config
+from app.services.redis import get_redis
 
 
-def create_access_token(user_id: UUID) -> str:
+async def create_access_token(user_id: UUID) -> str:
     expire = datetime.now(timezone.utc) + timedelta(minutes=config.jwt.access_expire_mins)
     payload = {
         "sub": str(user_id),
@@ -19,7 +20,15 @@ def create_access_token(user_id: UUID) -> str:
                       algorithm=config.jwt.algorithm)
 
 
-def verify_access_token(token: str) -> Union[UUID, None]:
+async def store_token(user_id: UUID,
+                      token: str,
+                      expires_in: int = config.jwt.access_expire_mins * 60):
+
+    redis = await get_redis()
+    await redis.set(str(user_id), token, ex=expires_in)
+
+
+async def decode_token(token: str) -> Union[UUID, None]:
     try:
         payload = jwt.decode(token,
                              config.jwt.secret_key,
@@ -29,3 +38,18 @@ def verify_access_token(token: str) -> Union[UUID, None]:
 
     except (JWTError, ValueError):
         return None
+
+
+async def verify_access_token(token: str) -> UUID | None:
+    user_id = decode_token(token)
+
+    if not user_id:
+        return None
+
+    redis = await get_redis()
+    stored_token = await redis.get(str(user_id))
+
+    if stored_token != token:
+        return None
+
+    return user_id
