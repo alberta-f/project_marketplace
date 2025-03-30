1f45b4398a2b2132f06fc53cfa9fa184f29980b5
alberta-f <delyasalakhova@gmail.com>
2025-03-30 20:40:11 +0300

    add middleware

---
+++ app/middlewares/auth.py
@@ -0,0 +1,27 @@
+from sqlalchemy.future import select
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.responses import Response
+
+from app.db.session import async_session_maker
+from app.models.models import User
+from app.services.jwt import verify_access_token
+
+
+class AuthMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request, call_next) -> Response:
+        request.state.user = None
+        token = request.cookies.get('access_token')
+
+        if token:
+            user_id = await verify_access_token(token)
+
+            if user_id:
+                async with async_session_maker() as session:
+                    res = await session.execute(select(User).where(User.id == user_id))
+                    user = res.scalar_one_or_none()
+
+                    if user:
+                        request.state.user = user
+
+        response = await call_next(request)
+        return response
