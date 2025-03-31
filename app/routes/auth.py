99db80e1737705a5a84a1f8e6d421271a43a80b7
alberta-f <delyasalakhova@gmail.com>
2025-03-31 15:06:28 +0300

    рефакторинг под новые функции токенов + создание токена для подьверждения почты + подтверждение почты

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, Request, Response
+from fastapi import APIRouter, Depends, HTTPException, Query, Request, Response
 from fastapi.responses import JSONResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
@@ -8,7 +8,7 @@ from app.db.session import get_db_session
 from app.models.models import User
 from app.schemas.user import UserCreate, UserLogin, UserRead
 from app.services.auth import hash_paasword, verify_password
-from app.services.jwt import create_access_token, delete_token, store_token
+from app.services.jwt import create_token, delete_token, verify_token
 
 auth_router = APIRouter()
 
@@ -35,7 +35,36 @@ async def register(data: UserCreate,
     await db_session.commit()
     await db_session.refresh(user)
 
-    return user
+    token = await create_token(user_id=user.id,
+                         token_type='email')
+
+    confirm_link = f"http://localhost:8000/auth/confirm-email?token={token}"
+
+    return {"user": user, "confirm_link": confirm_link}
+
+
+@auth_router.get('/confirm_email')
+async def confirm_email(
+    token: str = Query(...),
+    db: AsyncSession = Depends(get_db_session)
+):
+
+    user_id = await verify_token(token=token,
+                                 exc_token_type='email')
+
+    if not user_id:
+        raise HTTPException(status_code=400,
+                            detail='Invalid or expired token')
+
+    user = await db.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404,
+                            detail='User not found')
+
+    user.is_active = True
+    await db.commit()
+
+    return {"detail": "Email confirmed successfully ✅"}
 
 
 @auth_router.post('/login', response_model=UserRead)
@@ -51,8 +80,7 @@ async def login(
     if not user or not verify_password(data.password, user.hashed_password):
         raise HTTPException(status_code=401, detail='Invalid credentials')
 
-    token = await create_access_token(user.id)
-    await store_token(user.id, token)
+    token = await create_token(user.id, token_type='access')
 
     response.set_cookie(
         key='access_token',
@@ -79,10 +107,11 @@ async def get_me(request: Request):
 
 @auth_router.post('/logout')
 async def logout(request: Request, response: Response):
-    user = request.state.user
+    token = request.cookies.get("access_token")
 
-    if user:
-        await delete_token(user.id)
+    if token:
+        redis_key = f"access:{token}"
+        await delete_token(redis_key)
 
     response = JSONResponse(content={"detail": "Logged out"})
     response.delete_cookie("access_token")
