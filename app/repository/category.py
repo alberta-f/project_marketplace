e4f65879c02c5bc87df67994e01b46983aff7a57
alberta-f <delyasalakhova@gmail.com>
2025-04-10 18:47:37 +0300

    category repo, schemas, service

---
+++ app/repository/category.py
@@ -0,0 +1,20 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models import Category
+from app.repository.db_base import DBRepository
+
+
+class CategoryRepository(DBRepository[Category]):
+    def __init__(self, db: AsyncSession):
+        super().__init__(db, Category)
+
+
+    async def get_by_name(self, name: str):
+        stmt = select(Category).where(Category.name == name)
+        result = await self.db.execute(stmt)
+        return result.scalar_one_or_none()
+
+
+    async def get_all_categories(self):
+        return await self.db.scalars(select(Category))
---
+++ app/services/category.py
@@ -0,0 +1,35 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.exceptions.category import CategoryAlreadyExistsException, CategoryNotFoundException
+from app.models.category import Category
+from app.repository.category import CategoryRepository
+from app.schemas.category import CategoryCreate
+
+
+class CategoryService:
+    def __init__(self, db: AsyncSession):
+        self.category_repository = CategoryRepository()
+        self.db = db
+
+    async def create(self, data: CategoryCreate):
+        existing = await self.category_repository.get_by_name(data.name)
+        if existing:
+            raise CategoryAlreadyExistsException()
+
+        category = Category(name=data.name)
+        return await self.category_repository.create(category)
+
+    async def get(self, category_id):
+        category = await self.category_repository.get_by_id(category_id)
+
+        if not category:
+            raise CategoryNotFoundException()
+
+        return category
+
+    async def list(self):
+        return await self.category_repository.get_all_categories()
+
+    async def delete(self, category_id):
+        category = await self.category_repository.get_by_id(category_id)
+        await self.category_repository.delete(category)
