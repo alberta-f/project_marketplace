30b2459d52a0c63fa359197eefef1d7977a30f47
alberta-f <delyasalakhova@gmail.com>
2025-04-10 18:45:45 +0300

    get_services -> в свои файлы

--- app/dependencies.py
+++ app/dependencies.py
@@ -2,19 +2,20 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.database import get_db
-from app.services.security import SecurityService
+from app.services.mail import MailService, get_mail_service
+from app.services.security import SecurityService, get_security_service
 from app.services.user import UserService
 
 
 async def get_user_service(
     db: AsyncSession = Depends(get_db),
+    security: SecurityService = Depends(get_security_service),
+    mail: MailService = Depends(get_mail_service)
 ) -> UserService:
     security = SecurityService()
 
     return UserService(
         db=db,
-        security_service=security,
+        security=security,
+        mail=mail
     )
-
-def get_security_service():
-    return SecurityService()
