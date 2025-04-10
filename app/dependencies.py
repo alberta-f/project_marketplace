611f02c375162cea8c55e4a28be4bfe706011140
alberta-f <delyasalakhova@gmail.com>
2025-04-10 21:35:02 +0300

    dependencies

--- app/dependencies.py
+++ app/dependencies.py
@@ -2,8 +2,10 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.database import get_db
+from app.services.article import ArticleService
 from app.services.category import CategoryService
 from app.services.mail import MailService
+from app.services.minio import MinioService
 from app.services.security import SecurityService
 from app.services.token import TokenService
 from app.services.user import UserService
@@ -19,6 +21,11 @@ def get_mail_service():
 
 def get_security_service():
     return SecurityService()
+
+
+def get_minio_service():
+    return MinioService()
+
 
 def get_user_service(
     db: AsyncSession = Depends(get_db),
@@ -37,3 +44,11 @@ def get_category_service(
         db: AsyncSession = Depends(get_db),
 ):
     return CategoryService(db)
+
+
+def get_article_service(
+        db: AsyncSession = Depends(get_db),
+        minio_service: MinioService = Depends(get_minio_service),
+        category_service: CategoryService = Depends(get_category_service),):
+
+    return ArticleService(db, minio_service, category_service)
